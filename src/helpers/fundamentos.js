ab7ff2311b3c569071f66c83c64f7057d2022f49
lucascorreaa <lucas.lca14@gmail.com>
2024-07-06 15:46:23 -0300

    feat: added console methods and join string with variable

--- src/helpers/fundamentos.js
+++ src/helpers/fundamentos.js
@@ -1,1 +1,11 @@
 console.log("Oi Nedola")
+
+let error = "Não deu certo Nerdola"
+let info = "Info"
+
+console.error("error", error)
+console.error(`error ${error}`)
+console.info("Nerd", info)
+console.clear("console clean")
+
+console.log("voltei")
